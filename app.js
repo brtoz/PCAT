fb30f938d2b6f455a5145aec88560983c17c23bb
Berat <beratozfidan@hotmail.com>
2024-04-19 16:17:38 +0300

    Files Updated

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 
+// TEMPLAT ENGINE
+app.set('view engine', 'ejs');
+
 // Middleware
 app.use(express.static('public'));
 
+//ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
 });
 
 const port = 3000;
